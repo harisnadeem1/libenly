95cf8550a2bac7b81c85c24725609609a3755178
Muhammad Haris Nadeem <bscs21062@itu.edu.pk>
2026-06-22 01:44:31 +0500

    origin updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 // Allowed origins (SAFE DEV + FUTURE READY)
 const allowedOrigins = [
-  "http://localhost:5173"
+  "http://localhost:5173" , "https://libenly.vercel.app/"
   // later add:
   // "https://your-frontend-domain.com"
 ];
